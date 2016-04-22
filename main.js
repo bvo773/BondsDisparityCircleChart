43af78863f200a9480cf40398500c6b2a4c05de5
bvo773 <binhvo07@gmail.com>
2016-04-22 01:45:17 -0400

    Added files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+//Method to create the chart
+function makeHighchartsObject(){
+$(function () {
+
+    // Radialize the colors
+    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    });
+
+    // Build the chart
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'A closer look to Hartford Bond Arrest by Percentage breakdown'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        series: [{
+            name: 'Race',
+            data: [
+                { name: 'Black', y: 11.96 },
+                {
+                    name: 'White',
+                    y: 20.42,
+                    sliced: true,
+                    selected: true
+                },
+                { name: 'Hispanic', y:5.08 },
+                { name: 'Asian', y: 5.35 }, 
+                { name: 'Others', y: 57.19 }
+            ]
+        }]
+    });
+});
+}
+
+
---
+++ main1.js
@@ -0,0 +1,47 @@
+//Method to create the chart
+function makeHighchartsObject1(){
+$(function () {
+    $('#container1').highcharts({
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: '% of each race population in Hartford'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Race',
+            data: [
+                ['White', 16.0],
+                ['Black', 35.0],
+                {
+                    name: 'Hispanic',
+                    y: 44.0,
+                    sliced: true,
+                    selected: true
+                },
+                ['Others', 5.0]
+            ]
+        }]
+    });
+});
+}

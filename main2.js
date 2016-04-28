a9f044852edf478ef331e2202df264e407dc1f07
bvo773 <binhvo07@gmail.com>
2016-04-27 20:33:21 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -25,7 +25,7 @@ $(function () {
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:} dollars</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:} /100</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
